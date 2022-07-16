2fa8c7d2749606bf366729df5d51cacc9cf1d721
KrutikaBhatt <krutikabhatt222@gmail.com>
2022-07-16 12:45:00 +0530

    leaderboard initialization

---
+++ src/Components/LoyaltyConstruct/LeaderBoard.jsx
@@ -0,0 +1,11 @@
+import { Box, makeStyles, Typography, Button, Grid } from '@material-ui/core';
+
+const LeaderBoard = ({ match, history }) => {
+    return (
+        <>
+        <h2>Hello from leaderboard</h2>
+        </>
+    );
+}
+
+export default LeaderBoard;
