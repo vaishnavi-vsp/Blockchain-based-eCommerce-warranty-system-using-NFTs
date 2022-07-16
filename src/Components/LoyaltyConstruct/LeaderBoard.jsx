92e6af0e70b0f7ee0041a359eca499f853127103
KrutikaBhatt <krutikabhatt222@gmail.com>
2022-07-17 01:22:37 +0530

    leaderboard work in progress

--- src/Components/LoyaltyConstruct/LeaderBoard.jsx
+++ src/Components/LoyaltyConstruct/LeaderBoard.jsx
@@ -1,9 +1,26 @@
 import { Box, makeStyles, Typography, Button, Grid } from '@material-ui/core';
-
+import '../../App.css';
+import './style.css';
+import './table.css';
+import avatar_movement from '../../assets/avatars/dance.gif';
+import Board from './Board';
 const LeaderBoard = () => {
     return (
         <>
-        <h2>Hello from leaderboard</h2>
+        <div className='container d_flex'>
+            <div className="avatar">
+                <h1>Krutika Bhatt</h1>
+                <p>Supercoins: 300+</p>        
+                <img src={avatar_movement} height="450" className="avatar-img"/>
+                
+            </div>
+          <section className='homeSlide contentWidth'>
+            <div className='container'>
+              <Board />
+            </div>
+          </section>
+
+        </div>
         </>
     );
 }
---
+++ src/Components/LoyaltyConstruct/Profiles.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function profiles({ Leaderboard }) {
+  return (
+        <div id="profile">
+            {Item(Leaderboard)}
+        </div>
+  )
+}
+
+function Item(data){
+    return (
+
+        <>
+        <div className="table">
+            {
+                data.map((value, index) => (
+                    <div className="flex" key={index}>
+                        <div className="item">
+                            <img src={value.img} alt="" />
+            
+                            <div className="info">
+                                <h3 className='name text-dark'>{value.name}</h3>    
+                                <span>{value.location}</span>
+                            </div>                
+                        </div>
+                        <div className="item">
+                            <span>{value.score}</span>
+                        </div>
+                    </div>
+                    )
+                )
+            }
+            </div>
+        </>
+
+        
+    )
+}
